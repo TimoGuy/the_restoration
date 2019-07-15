701dd3647ca28d4904afe455b37e1cfda3ef5a8f
Hawsoo <Hawsoo@users.noreply.github.com>
2019-07-14 22:39:41 -0700

    Found a fix for texture-bleeding... changed trilinear blending to no blending in the texture

--- src/Lib/Texture.cpp
+++ src/Lib/Texture.cpp
@@ -90,8 +90,8 @@ void Texture::GenOpenGLTex(unsigned char* imgData, int comp)
 
     // Use mipmaps and enable texture clamping
 //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);       // TODO: get mipmapping, eh!
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);			// GL_NEAREST is used so the tilesets don't do a trilinear blend and cause neighboring cells of the texture to bleed over
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
